a1cb61d4ffcc7459e14dc196bf48f4974fd15353
3cordguy <jweaver911@gmail.com>
2023-08-27 16:56:55 -0400

    slows down milestone sheen
    - minor changes to milestone animation

--- src/collectibles.lua
+++ src/collectibles.lua
@@ -1,25 +1,33 @@
 function i_milestone_anims()
-	frames={18, 18, 18, 18, 18, 19, 20, 21, 21}
-	timing=.15
+	mile_frames={18,18,18,18,18,18,18,18,18,18,18,18, 19, 20, 21, 22, 22}
+	mile_timing=.3
+	mile_delay=20
+	mile_tick=0
 	prev_milestone_x=128
 	milestones={}
 
 	add(milestones, {
+		x=40,
+		y=40,
+		f=1,
+		animate=tick_frames
+	})
+
+	add(milestones, {
 		x=rnd_between(prev_milestone_x, prev_milestone_x + 256),
 		y=mid(rnd(20),rnd(50),rnd(80)),
-		f=frames[1]
+		f=1,
+		animate=tick_frames
 	})
 end
 
 function u_milestone_anims()
 	--animate milestone
 	for k,mile in ipairs(milestones) do
-		mile.f=mile.f+timing
-
-		if mile.f > frames[9] then
-			mile.f = frames[1]
-		end
+		mile:animate()
 	end
+
+
 	-- handle collisions
 	milestone_collide()
 
@@ -27,7 +35,8 @@ function u_milestone_anims()
 		add(milestones, {
 			x=rnd_between(prev_milestone_x + 128, prev_milestone_x + 258),
 			y=mid(rnd(30),rnd(50),rnd(80)),
-			f=frames[1]
+			f=1,
+			animate=tick_frames
 		})
 	end
 end
@@ -35,6 +44,14 @@ end
 
 function d_milestone_anims()
 	for k,mile in ipairs(milestones) do
-		spr(flr(mile.f), mile.x, mile.y)
+		print(flr(mile.f), mile.x, mile.y+8)
+		spr(mile_frames[flr(mile.f)], mile.x, mile.y)
+	end
+end
+
+function tick_frames(self)
+	self.f += mile_timing
+	if (self.f > #mile_frames) then
+		self.f = 1
 	end
 end
--- src/utils.lua
+++ src/utils.lua
@@ -2,3 +2,13 @@
 function rnd_between(min, max)
 	return flr(rnd(max-min+1))+min
 end
+
+function delay_frames(frames, callback)
+	local t = 0
+	return function()
+		t += 1
+		if t >= frames then
+			callback()
+		end
+	end
+end
